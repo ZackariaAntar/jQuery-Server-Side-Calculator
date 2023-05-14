8e21035c5c65e86c13bb9125526b4b7cc4e49cdd
Zackaria Antar <antarzack@gmail.com>
2023-05-14 17:37:07 -0500

    attempted to build clear history functionality.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,97 +1,126 @@
 $(document).ready(onReady);
-
-
+// setting up global variables to store
 let expression;
 let operator;
 let tracker;
 
-
-function onReady(){
-    console.log('loaded');
-    $('.input').on('click', displayInput)
-    $('#clear').on('click', wipeTheInputs)
-    $('#calc').on('submit', postToServer)
-    // getFromServer()
+function onReady() {
+	// registering our event handlers
+	console.log("loaded");
+	$(".input").on("click", displayInput);
+	$("#clear").on("click", wipeTheInputs);
+	$("#calc").on("submit", checkForErrors); // on submit run through checkForErrors before postToServer for exception handling.
+	getFromServer();
 }
 
-function displayInput(event){
-    event.preventDefault();
-    let testParam = new RegExp(/^[a-z0-9.]+$/i);
-    let validate = testParam.test($(this).attr("value"));
-    let input = $(this).attr("value");
-    tracker+= input
-    console.log('clicked');
-    console.log(input);
-    console.log(tracker);
-    $("#exp").val(tracker.replace("undefined",''));
-    if (!validate){
-        operator = $(this).attr("value");
-        console.log("this is an operator", operator);
-    }
-
-    // conditional to grab the operator which might be helpful to also send on server side for calc conditional.
+function displayInput(event) {
+	// function to capture the values of the .input buttons and display them in the input field.
+	event.preventDefault();
+	let testParam = new RegExp(/^[0-9.]+$/i); // storing a regular expression to differentiate between numeric and operator values in the testParam variable.
+	let validate = testParam.test($(this).attr("value")); // testing the .input value against the testParam and storing the boolean value in the validate variable.
+	let input = $(this).attr("value"); // storing the .input value in the input variable.
+	tracker += input; // concatenating the .inputs and storing them in the global tracker value.
+	$("#exp").val(tracker.replace("undefined", "")); // setting the value of the input field dynamically using the concatenation from tracker+=input and replacing the undefined that also appears.
+	if (!validate) {
+		// conditional that triggers if one of the .input values does not pass the regex validation test.
+		operator = $(this).attr("value"); //store the operator value in the global operator variable if not true (aka doesn't pass)
+		console.log("this is an operator", operator);
+	}
 }
 
+function checkForErrors(event) {
+	// exception handling using a regular expression that attempts to ensure only valid athrithmetic sequences are submitted.
+	event.preventDefault();
+	let checkParam = new RegExp(/^[-]?\d+(\.\d+)?([-+/*]?[-]?\d+(\.\d+)?)*$/gm);
+	let entry = $("#exp").val();
+	console.log(entry);
+	let passParam = checkParam.test(entry);
+	if (!passParam) {
+		// tried || tracker.replace("undefined", "") <= 1
+		alert(
+			`Yoooooo, you good? ${tracker.replace(
+				"undefined",
+				""
+			)} is invalid. Please try again.`
+		);
+		tracker = "";
+		$("#exp").val(tracker);
+	} else {
+		postToServer();
+	}
+	//somthing like:
+	//if ($('#exp').val() !regex){
+	//alert(try again you little knuckle head.)
+	//$('#exp').empty()
+	//}else postToServer()
+}
 
-function postToServer(event){
-    event.preventDefault()
-    expression = $('#exp').val()
-    $.ajax({
-        method: 'POST',
-        url: '/calc',
-        data:{
-            exp: expression,
-            oper: operator,
-        }
-    }).then(function(response){
-        getFromServer();
-    }).catch(function(error){
-        console.log("whoops!", error);
-    })
-
-
+function postToServer() {
+	// once a valid input has been submitted post the input to the server.
+	console.log("posted");
+	expression = $("#exp").val();
+	$.ajax({
+		method: "POST",
+		url: "/calc",
+		data: {
+			exp: expression, // entire input field string
+			oper: operator, // operator used during calculation.
+		},
+	})
+		.then(function (response) {
+			// after posting try to get data back from server.
+			tracker = "";
+			$("#exp").val("");
+			getFromServer();
+		})
+		.catch(function (error) {
+			console.log("whoops!", error);
+		});
 }
 
 function getFromServer() {
-    $.ajax({
+	$.ajax({
 		method: "GET",
 		url: "/calc",
-	}).then(function (response) {
+	})
+		.then(function (response) {
+			// after data has returned, use it in the renderToDOM function.
 			renderToDOM(response);
-		}).catch(function (error) {
-			console.log("whoops!", error);
+		})
+		.catch(function (error) {
+			// console.log("whoops!", error);
+			renderToDOM(response);
 		});
-
-
 }
 
-function renderToDOM(evaluations){
-    let current = evaluations[evaluations.length -1]
-
-    $('#that-number').empty()
-    $('#that-number').text(current.eval)
-    $("#history").append(
-		`<li class="history">${current.history} = ${current.eval} </li>`
-	);
+function renderToDOM(evaluations) {
+	let current = evaluations[evaluations.length - 1]; // setting current to the last object in the array stored on the server.
+	$("#that-number").empty(); // clear the text in the area were going to put the result in.
+	$("#that-number").text(current.ev); // put the result in that area.
+	$("#history").append(`<li class="history"> </li>`); // add an li element to the DOM
+	$("li").last().empty(); // empty the text in the last li element.
+	$("li").last().text(current.history); // put the expression string text in the last li element on the DOM.
 }
 
-function wipeTheInputs(){
-    $("#exp").val("");
+function wipeTheInputs(event) {
+	// function to run when the clear button is pressed.
+	event.preventDefault();
+	$("li").last().remove(); //attempts to remove the last li element on the DOM.
+	$.ajax({
+		// ajax delete call.
+		method: "DELETE",
+		url: "/calc",
+	})
+		.then(function (response) {
+			// get the data back from the server after the delete call is complete.
+			getFromServer();
+		})
+		.catch(function (error) {
+			console.log("Whoopsies!", error);
+		});
 }
 
+// let index = $('li:last-child').data('index')
 
-
-
-
-// keeping these here for easy reference.
-
-// <form id="calc">
-//      <input type="number" id="first">
-//             <button value="+" id="plus" >+</button>
-//             <button value="-" id="minus" >-</button>
-//             <button value="*" id="times" >*</button>
-//             <button value="/" id="divide" >/</button>
-//      <input type="number" id="second">
-//             <button value="=" type="submit" id="equals">=</button>
-//             <button value="c" type="reset" id="clear">C</button>
-// </form>
+// might not need to use this since I'm not trying to delete by clicking on the actual list item?
+// intuitively the clear button should .pop the last thing added to the array on server and then rerender.
--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require("express"); // include express
 const bodyParser = require("body-parser"); // include body-parser
 
-
 const app = express(); // changing how we access express to use app for methods instead.
 
 const port = 5000; // registering port we want to listen on.
@@ -10,51 +9,64 @@ app.use(express.static("server/public")); // use the static files in the public 
 app.use(bodyParser.urlencoded({ extended: true })); // configuuring options for body-parser
 
 const answer = [];
+const history = [];
 const cache = {};
 
-app.get('/calc', function(req, res) {// establishing the server path for data to travel on. Where to get it from.
-  console.log('Requesting Calculator logic');
+app.get("/calc", function (req, res) {
+	// establishing the server path for data to travel back to the client.
+	console.log("Requesting Calculator logic");
 
-  res.send(answer);
+	res.send(answer); // sends the ansder array of objects back to the client side
 });
 
 app.post("/calc", function (req, res) {
-	// // establishing the server path for response data to travel on. Where to take it to.
+	// // establishing the server path for post data to travel on.
 	console.log("Calculator data was sent", req.body);
+	history.push(req.body); // saving the integrity of the inputs
 	// string.substring(0, string.indexOf(character)); //// before the special character
 	// string.substring(string.indexOf(character) + 1) //// after special character.
 
-	if (req.body.oper === "+") {
-		let history = req.body.exp
-		let firstNum = req.body.exp.substring(0, req.body.exp.indexOf("+"))
-		let nextNum = req.body.exp.substring(req.body.exp.indexOf("+") + 1);
-		cache.eval = (firstNum * 1) + (nextNum * 1);
-		cache.history = history
-		answer.push(cache);
+	// logic below compares the value of the oper property against hard coded operator strings
+	// once the operator is matched, then complete the calculation by capturing the numbers before and after the operator
+	// store the result in the cache.ev property, store the original expression in the history property, and push it to the array to be returned in the GET call.
 
+	if (req.body.oper === "+") {
+		let history = req.body.exp;
+		let firstNum = req.body.exp.substring(0, req.body.exp.indexOf("+"));
+		let nextNum = req.body.exp.substring(req.body.exp.indexOf("+") + 1);
+		cache.ev = firstNum * 1 + nextNum * 1;
+		cache.history = history;
+		answer.push(cache);
 	} else if (req.body.oper === "-") {
 		let history = req.body.exp;
 		let firstNum = req.body.exp.substring(0, req.body.exp.indexOf("-"));
 		let nextNum = req.body.exp.substring(req.body.exp.indexOf("-") + 1);
-		cache.eval = firstNum - nextNum ;
+		cache.ev = firstNum - nextNum;
 		cache.history = history;
 		answer.push(cache);
 	} else if (req.body.oper === "*") {
 		let history = req.body.exp;
 		let firstNum = req.body.exp.substring(0, req.body.exp.indexOf("*"));
 		let nextNum = req.body.exp.substring(req.body.exp.indexOf("*") + 1);
-		cache.eval = firstNum * nextNum;
+		cache.ev = firstNum * nextNum;
 		cache.history = history;
+
 		answer.push(cache);
 	} else if (req.body.oper === "/") {
 		let history = req.body.exp;
 		let firstNum = req.body.exp.substring(0, req.body.exp.indexOf("/"));
 		let nextNum = req.body.exp.substring(req.body.exp.indexOf("/") + 1);
-		cache.eval = firstNum / nextNum;
+		cache.ev = firstNum / nextNum;
 		cache.history = history;
 		answer.push(cache);
 	}
 
+	res.sendStatus(201);
+});
+
+app.delete("/calc", function (req, res) {
+	console.log("Delete request was sent");
+	answer.pop(); // remove the last object in the answer array
 	res.sendStatus(201);
 });
 
